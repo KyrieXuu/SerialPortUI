4d53f6e0aa66e865e79344d55554233bdc9a1741
KyrieXuu <593065220@qq.com>
2024-01-17 21:00:53 +0800

    补充了mqtt重连机制

--- mqttclient.cpp
+++ mqttclient.cpp
@@ -34,8 +34,32 @@ void MQTTClient::connect(QString hostname, int port){
     m_client.setHostname(hostname);
     m_client.setPort(static_cast<quint16>(port));
     m_client.connectToHost();
+    QObject::connect(&m_client, &QMqttClient::stateChanged, [&](QMqttClient::ClientState state) {
+        if (state == QMqttClient::Connected)
+        {
+            qDebug() << "MQTT connection is established.";
+        }
+        else if (state == QMqttClient::Disconnected)
+        {
+            qDebug() << "MQTT connection is disconnected.";
+            retryConnect();
+        }
+    });
 }
 
 void MQTTClient::disconnect(){
     m_client.disconnectFromHost();
+}
+
+void MQTTClient::retryConnect() {
+    while(m_client.state() == QMqttClient::Disconnected){
+        m_client.connectToHost();
+        qDebug() << "Retrying MQTT connection...";
+        currentReconnectAttempts++;
+        if(currentReconnectAttempts > maxReconnectAttempts){
+            currentReconnectAttempts = 0;
+            qDebug() << "超过重连次数，重连失败";
+            break;
+        }
+    }
 }
--- mqttclient.h
+++ mqttclient.h
@@ -15,9 +15,13 @@ public:
     void publishJSONMessage(QString topic, QString msg, int qos);
     void connect(QString hostname, int port);
     void disconnect();
+    void retryConnect();
 
 private:
     QMqttClient m_client;
+    // 设置最大重连次数
+    int maxReconnectAttempts = 5;
+    int currentReconnectAttempts = 0;
 };
 
 #endif // MQTTCLIENT_H
